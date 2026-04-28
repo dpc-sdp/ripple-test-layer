2c366ff3b3964a0aad172d45916aa8a66d40b9bb
dylankelly <dylan.kelly@dpc.vic.gov.au>
2026-04-28 10:59:04 +1000

    chore: move data to seperate file

--- layers/example/app/pages/example.vue
+++ layers/example/app/pages/example.vue
@@ -62,54 +62,6 @@
 </template>
 
 <script setup lang="ts">
-import { primaryNav, footerNav, footerLinks } from './../../data/nav'
+import { primaryNav, footerNav, footerLinks, heroHeader, cards } from './../../data/nav'
 
-
-const heroHeader = {
-  title: 'Find, connect, shape your Victoria',
-  summary:
-    "Access grants and services, find out what's on in Victoria and have your say on government decisions.",
-  links: {
-    title: 'Popular searches',
-    items: [
-      { text: 'Jobs and careers', url: '#first' },
-      { text: 'Working with Children Check', url: '#second' },
-      { text: 'NDIS Worker Screening Check', url: '#third' },
-      { text: 'Contact the Victorian Government', url: '#fourth' }
-    ]
-  }
-}
-
-const cards = [
-  {
-    url: 'https://www.vic.gov.au',
-    title: 'Small Business Ventilation Program',
-    image: {
-      src: '/image.jpg',
-      alt: 'image'
-    },
-    content:
-      '<p>Funding for public-facing small businesses to purchase equipment and upgrades to improve ventilation and reduce the spread of COVID-19 and boost customer confidence.</p>'
-  },
-  {
-    url: 'https://www.vic.gov.au',
-    title: 'Small Business Ventilation Program',
-    image: {
-      src: '/image.jpg',
-      alt: 'image'
-    },
-    content:
-      '<p>Funding for public-facing small businesses to purchase equipment and upgrades to improve ventilation and reduce the spread of COVID-19 and boost customer confidence.</p>'
-  },
-  {
-    url: 'https://www.vic.gov.au',
-    title: 'Small Business Ventilation Program',
-    image: {
-      src: '/image.jpg',
-      alt: 'image'
-    },
-    content:
-      '<p>Funding for public-facing small businesses to purchase equipment and upgrades to improve ventilation and reduce the spread of COVID-19 and boost customer confidence.</p>'
-  }
-]
 </script>
--- layers/example/data/nav.ts
+++ layers/example/data/nav.ts
@@ -105,3 +105,52 @@ export const footerLinks = [
   { text: 'Sitemap', url: '#' },
   { text: 'Accessibility', url: '#' }
 ]
+
+
+export const heroHeader = {
+  title: 'Find, connect, shape your Victoria',
+  summary:
+    "Access grants and services, find out what's on in Victoria and have your say on government decisions.",
+  links: {
+    title: 'Popular searches',
+    items: [
+      { text: 'Jobs and careers', url: '#first' },
+      { text: 'Working with Children Check', url: '#second' },
+      { text: 'NDIS Worker Screening Check', url: '#third' },
+      { text: 'Contact the Victorian Government', url: '#fourth' }
+    ]
+  }
+}
+
+export const cards = [
+  {
+    url: 'https://www.vic.gov.au',
+    title: 'Small Business Ventilation Program',
+    image: {
+      src: '/image.jpg',
+      alt: 'image'
+    },
+    content:
+      '<p>Funding for public-facing small businesses to purchase equipment and upgrades to improve ventilation and reduce the spread of COVID-19 and boost customer confidence.</p>'
+  },
+  {
+    url: 'https://www.vic.gov.au',
+    title: 'Small Business Ventilation Program',
+    image: {
+      src: '/image.jpg',
+      alt: 'image'
+    },
+    content:
+      '<p>Funding for public-facing small businesses to purchase equipment and upgrades to improve ventilation and reduce the spread of COVID-19 and boost customer confidence.</p>'
+  },
+  {
+    url: 'https://www.vic.gov.au',
+    title: 'Small Business Ventilation Program',
+    image: {
+      src: '/image.jpg',
+      alt: 'image'
+    },
+    content:
+      '<p>Funding for public-facing small businesses to purchase equipment and upgrades to improve ventilation and reduce the spread of COVID-19 and boost customer confidence.</p>'
+  }
+]
